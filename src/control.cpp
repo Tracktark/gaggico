37e074e900eb0befd3a3a2bec231b531ee6fc72b
Tracktark <rikoczz@gmail.com>
2024-01-14 22:17:10 +0100

    Added PID control to temperature

--- src/control.cpp
+++ src/control.cpp
@@ -1,23 +1,23 @@
 #include "control.hpp"
+#include <pico/time.h>
 #include "hardware.hpp"
-#include "messages.hpp"
-#include <cstdio>
+#include "pid.hpp"
 using namespace control;
 
-bool boiler_enabled = false;
+
+PID heater_pid(0.087, 0.00383, 0.49416, 0, 1);
+
+bool heater_enabled = false;
 void control::set_boiler_enabled(bool enabled) {
-    boiler_enabled = enabled;
-    if (!enabled) {
+    heater_enabled = enabled;
+    if (enabled) {
+        heater_pid.reset(hardware::read_temp());
+    } else {
         hardware::set_heater(0);
     }
 }
 
-bool pump_enabled = false;
 void control::set_pump_enabled(bool enabled) {
-    // pump_enabled = enabled;
-    // if (!enabled) {
-    //     hardware::set_pump(0);
-    // }
     hardware::set_pump(enabled);
 }
 
@@ -26,31 +26,25 @@ void control::set_target_pressure(float pressure) {
     target_pressure = pressure;
 }
 
-float target_temp = 0;
 void control::set_target_temperature(float temperature) {
-    target_temp = temperature;
+    heater_pid.set_target(temperature);
 }
 
 void control::update() {
-    float curr_temp = hardware::read_temp();
+    static absolute_time_t next_update_time = nil_time;
+    if (!time_reached(next_update_time))
+        return;
+    next_update_time = make_timeout_time_ms(250);
+
     float curr_pressure = hardware::read_pressure();
-    // printf("Temp: %f\nPres: %f\n", curr_temp, curr_pressure);
 
-    if (boiler_enabled) {
-        if (curr_temp < target_temp) {
-            hardware::set_light(hardware::Steam, true);
-            hardware::set_heater(1);
-        } else {
-            hardware::set_light(hardware::Steam, false);
-            hardware::set_heater(0);
-        }
+    if (heater_enabled) {
+        float curr_temp = hardware::read_temp();
+
+        float heater_value = heater_pid.update(curr_temp);
+        hardware::set_heater(heater_value);
+
+        bool temp_close_enough = fabs(heater_pid.get_target() - curr_temp) < 1;
+        hardware::set_light(hardware::Steam, temp_close_enough);
     }
-
-    // if (pump_enabled) {
-    //     if (curr_pressure < target_pressure) {
-    //         hardware::set_pump(1);
-    //     } else {
-    //         hardware::set_pump(0);
-    //     }
-    // }
 }
---
+++ src/include/pid.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <algorithm>
+#include <pico/time.h>
+#include <cmath>
+
+class PID {
+    float kP, kI, kD;
+    float last_value = 0;
+    float accumulator = 0;
+
+    float outMin = 0, outMax = 0;
+
+    float target_value = 0;
+    absolute_time_t last_update_time {0};
+
+public:
+    constexpr explicit PID(float kP, float kI, float kD, float outMin, float outMax)
+        : kP(kP), kI(kI), kD(kD), outMin(outMin), outMax(outMax) {}
+
+    constexpr void set_target(float target) {
+        target_value = target;
+    }
+    constexpr float get_target() const {
+        return target_value;
+    }
+
+    float update(float curr_value) {
+        absolute_time_t curr_time = get_absolute_time();
+        float delta_time = absolute_time_diff_us(last_update_time, curr_time) / 1'000'000.0;
+        last_update_time = curr_time;
+
+        float error = target_value - curr_value;
+
+        float change_rate = -(curr_value - last_value) / delta_time;
+        last_value = curr_value;
+
+        accumulator += kI * error * delta_time;
+
+        accumulator = std::clamp(accumulator, outMin, outMax);
+
+        return std::clamp(error * kP + change_rate * kD + accumulator, outMin, outMax);
+    }
+
+    void reset(float curr_value) {
+        last_value = curr_value;
+        last_update_time = get_absolute_time();
+        accumulator = 0;
+    }
+};
