097c71046228f007035e09acf9dc9a7e9590540f
Tracktark <rikoczz@gmail.com>
2024-05-05 13:38:49 +0200

    Added codegen for serialization and deserialization of structs

--- src/network/impl/message.hpp
+++ src/network/impl/message.hpp
@@ -2,6 +2,7 @@
 
 #include <variant>
 #include <boost/pfr.hpp>
+#include "serde.hpp"
 #include "inttypes.hpp"
 
 template <typename T>
@@ -9,37 +10,6 @@ concept IncomingMsg = requires(T t) {
     { T::INCOMING_ID } -> std::convertible_to<i32>;
     { t.handle() } -> std::same_as<void>;
 };
-
-template <typename T>
-concept CustomReadMsg = IncomingMsg<T> && requires(T t, u8*& ptr) {
-    { t.read(ptr) } -> std::same_as<void>;
-};
-
-template <IncomingMsg Msg, std::size_t FieldCount, std::size_t CurrField>
-struct read_impl {
-    static void read(Msg& msg, u8*& data_ptr) {
-        read_val(data_ptr, boost::pfr::get<FieldCount-CurrField>(msg));
-        read_impl<Msg, FieldCount, CurrField-1>::read(msg, data_ptr);
-    }
-};
-
-template <IncomingMsg Msg, std::size_t FieldCount>
-struct read_impl<Msg, FieldCount, 0> {
-    static void read(Msg& msg, u8*& data_ptr) {}
-};
-
-
-template <CustomReadMsg Msg>
-void read(Msg& msg, u8*& data_ptr) {
-    msg.read(data_ptr);
-}
-
-template <typename Msg>
-void read(Msg& msg, u8*& data_ptr) {
-    constexpr std::size_t field_count = boost::pfr::tuple_size_v<Msg>;
-    read_impl<Msg, field_count, field_count>::read(msg, data_ptr);
-}
-
 
 template <typename Tuple, typename Variant>
 struct handle_impl;
@@ -49,7 +19,7 @@ struct handle_impl<std::variant<T, Ts...>, Variant> {
     static void handle(int msg_id, Variant& msg, u8*& data) {
         if (msg_id == T::INCOMING_ID) {
             T& t = msg.template emplace<T>();
-            read<T>(t, data);
+            read_struct<T>(t, data);
             t.handle();
         } else {
             handle_impl<std::variant<Ts...>, Variant>::handle(msg_id, msg, data);
--- src/network/impl/serde.hpp
+++ src/network/impl/serde.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <bit>
 #include <lwip/def.h>
+#include <boost/pfr.hpp>
 #include "inttypes.hpp"
 
 template <class>
@@ -43,4 +44,63 @@ inline void read_val(u8*& ptr, T& value) {
     } else {
         static_assert(dependent_false<T>, "read_val not implemented for this type");
     }
+}
+
+template <typename T>
+concept CustomRead = requires(T t, u8*& ptr) {
+    { t.read(ptr) } -> std::same_as<void>;
+};
+
+template <typename T, std::size_t FieldCount, std::size_t CurrField>
+struct read_impl {
+    static void read(T& val, u8*& data_ptr) {
+        read_val(data_ptr, boost::pfr::get<FieldCount-CurrField>(val));
+        read_impl<T, FieldCount, CurrField-1>::read(val, data_ptr);
+    }
+};
+
+template <typename T, std::size_t FieldCount>
+struct read_impl<T, FieldCount, 0> {
+    static void read(T& val, u8*& data_ptr) {}
+};
+
+template <CustomRead T>
+void read_struct(T& val, u8*& data_ptr) {
+    val.read(data_ptr);
+}
+
+template <typename T>
+void read_struct(T& val, u8*& data_ptr) {
+    constexpr std::size_t field_count = boost::pfr::tuple_size_v<T>;
+    read_impl<T, field_count, field_count>::read(val, data_ptr);
+}
+
+template <typename T>
+concept CustomWrite = requires(const T& t, u8*& ptr) {
+    { t.write(ptr) } -> std::same_as<void>;
+};
+
+template <typename T, std::size_t FieldCount, std::size_t CurrField>
+struct write_impl {
+    static void write(const T& val, u8*& data_ptr) {
+        write_val(data_ptr, boost::pfr::get<FieldCount-CurrField>(val));
+        write_impl<T, FieldCount, CurrField-1>::write(val, data_ptr);
+    }
+};
+
+template <typename T, std::size_t FieldCount>
+struct write_impl<T, FieldCount, 0> {
+    static void write(const T& val, u8*& data_ptr) {}
+};
+
+
+template <CustomWrite T>
+void write_struct(const T& val, u8*& data_ptr) {
+    val.write(data_ptr);
+}
+
+template <typename T>
+void write_struct(const T& val, u8*& data_ptr) {
+    constexpr std::size_t field_count = boost::pfr::tuple_size_v<T>;
+    write_impl<T, field_count, field_count>::write(val, data_ptr);
 }
