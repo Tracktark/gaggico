97a3026396838be58eda421033f4a4eefc37828b
Tracktark <rikoczz@gmail.com>
2025-08-20 11:23:06 +0200

    Change discovery response to `gaggico`

--- src/network/discovery.cpp
+++ src/network/discovery.cpp
@@ -6,12 +6,12 @@
 
 using namespace discovery;
 
-const char* RESPONSE = "pico here";
+const char* RESPONSE = "gaggico";
 static struct pbuf* resp_pbuf = nullptr;
 
 static void incoming_callback(void* arg, struct udp_pcb* pcb, struct pbuf* inp, const ip_addr_t* addr, u16_t port) {
     pbuf_free(inp);
-    udp_sendto(pcb, resp_pbuf, addr, PORT);
+    udp_sendto(pcb, resp_pbuf, addr, port);
 }
 
 void discovery::init() {
