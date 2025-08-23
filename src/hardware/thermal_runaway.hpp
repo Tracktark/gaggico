63b80e447dd38cc80aba47774377270d40e13b59
Tracktark <rikoczz@gmail.com>
2025-08-23 13:51:40 +0200

    Adjusted thermal runaway check parameters

--- src/hardware/thermal_runaway.hpp
+++ src/hardware/thermal_runaway.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <cmath>
-#include <hardware/timer.h>
+#include "inttypes.hpp"
+#include <cstdio>
 #include <pico/time.h>
-#include <pico/types.h>
 
 // Assume enabled heater can raise the temperature of water by at least HEATING_GAIN °C over DEADLINE_S seconds
 constexpr int HEATING_GAIN = 1;
@@ -14,7 +13,7 @@ struct ThermalRunawayCheck {
     float goal_temp = 0;
 
     bool has_fault(i32 heater_power, i32 pump_power, float temp) {
-        bool deadline_enabled = heater_power > 20 && pump_power <= 1;
+        bool deadline_enabled = heater_power > 90 && pump_power <= 1;
         if (!deadline_enabled) {
             deadline = nil_time;
             goal_temp = 0;
@@ -23,6 +22,8 @@ struct ThermalRunawayCheck {
 
         if (time_reached(deadline) || temp >= goal_temp) {
             if (temp < goal_temp) {
+                printf("Thermal runaway check failed, temp: %.2f, goal: %.2f, heater: %u%%, pump: %u%%\n",
+                       temp, goal_temp, heater_power, pump_power);
                 return true;
             }
             goal_temp = temp + HEATING_GAIN;
